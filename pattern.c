cc4b2d8065e35bb63ca6af2a048ce93d11b17c23
Sugam Mainali <sugammainali5@gmail.com>
2025-06-10 08:35:04 +0545

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// WAP to run a code of pattern
+int main(){
+	int i, j;
+	for (i=0; i<=4; i++){
+	printf(" ");
+		for (j=0; j<=4; j++){
+				
+		printf("*");
+		}
+	printf("\n");		
+	}
+	return 0;
+}
