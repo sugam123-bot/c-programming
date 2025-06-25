1ba047cc67171251d623db3b63946b5cd2c91d8c
Sugam Mainali <sugammainali5@gmail.com>
2025-06-25 22:11:39 +0545

    Add files via upload

---
+++ name in array.c
@@ -0,0 +1,11 @@
+//WAP that require a name from you by using array and strings
+#include<stdio.h>
+int main(){
+	int i;
+char name[20];
+printf("Enter your name:\n");
+scanf("%s", name);// only take a enter character before white space
+printf("%s\n", name);
+	return 0;
+	
+}
