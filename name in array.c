9450948be763fab2d7f13b65c0084288b52be23c
Sugam Mainali <sugammainali5@gmail.com>
2025-07-06 22:22:38 +0545

    Update name in array.c

--- name in array.c
+++ name in array.c
@@ -1,7 +1,6 @@
 //WAP that require a name from you by using array and strings
 #include<stdio.h>
 int main(){
-	int i;
 char name[20];
 printf("Enter your name:\n");
 scanf("%s", name);// only take a enter character before white space
