73fbb63f167455abd5762d1c7e187428c9064bd9
Sugam Mainali <sugammainali5@gmail.com>
2025-08-07 22:31:06 +0545

    strcat

---
+++ string using strcat.c
@@ -0,0 +1,12 @@
+//WAP to concatenate two strings using strcat() library functions
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char s1[10]="Happy";
+	char s2[20]="Birthday to you";
+	char s3[40]=" ";
+	printf("Chet Prsad Mainali\n");
+	printf("s1=%s\n s2=%s\n", s1, s2);
+	printf("\nstrcat(s1, s2) = %s\n", strcat(s1, s2));n
+	return 0;
+} 
