637500129f0110c44d3eadfa95a7e9a263d4bb28
Chet Prasad Mainali <chet.817402@bkmc.tu.edu.np>
2025-11-09 08:31:19 +0545

    Add files via upload
    graphic.circle

---
+++ graphic.circle.c
@@ -0,0 +1,16 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)"");
+
+    
+    circle(200,200,100);
+
+    getch();
+ closegraph();
+    return 0;
+}
+
